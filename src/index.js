67fcb72ee62387dec5123dcab79061fa7ef5e32a
Hiba <eng.lilil92@gmail.com>
2022-11-12 18:16:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //your code goes here 
+  const form = document.querySelector('form');
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const task = document.getElementById('new-task-description');
+    const textValue =task.value;
+    const priorety = document.getElementById('priorety');
+    const prioretyVal= priorety.options[priorety.selectedIndex].text;
+    addTask(textValue,prioretyVal);
+  })
 });
+
+
+
+function getColor(value){
+  if(value === 'high'){
+    return 'red';
+  }
+  if(value=== 'medium'){
+    return 'yellow';
+  }
+  if(value === 'low'){
+    return 'green';
+  }
+}
+
+function addTask(task,priority){
+  const listElement = document.createElement('li');
+  listElement.innerText = task;
+  listElement.style.backgroundColor = getColor(priority);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML=' <i class="fa fa-trash" ></i>'
+  deleteBtn.addEventListener('click',(e)=>{
+    e.target.parentNode.parentNode.remove();
+  })
+  
+  listElement.appendChild(deleteBtn);
+  const tasks = document.getElementById('tasks');
+  tasks.appendChild(listElement);
+}
